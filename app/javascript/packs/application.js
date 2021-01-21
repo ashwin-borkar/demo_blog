21cf6a66d45704c49dcc4282a5917136264aa126
ashwin-borkar <ashwinborkar1997@gmail.com>
2021-01-21 14:18:28 +0530

    add the datatble pagination feature

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,17 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+require("chartkick")
+require("chart.js")
+require("chartkick").use(require("highcharts"))
 
+import 'bootstrap'
+
+require('datatables.net-bs4')
+
+import $ from 'jquery';
+global.$ = jQuery;
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -19,3 +29,8 @@ require("channels")
 //= require popper
 //= require bootstrap-sprockets
 //= require_tree .
+
+import 'bootstrap'
+import './src/application.scss'
+window.jQuery = $;
+window.$ = $;
